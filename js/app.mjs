6f57b1d0ecfa086eb67247645434a52c00d50f2f
ParthNikam <76932381+ParthNikam@users.noreply.github.com>
2021-01-15 10:43:52 +0530

    Add files via upload

---
+++ js/app.mjs
@@ -0,0 +1,139 @@
+import {
+  addFontFromFile,
+  formatText,
+  addPaperFromFile
+} from './utils/helpers.mjs';
+import {
+  generateImages,
+  downloadAsPDF,
+  deleteAll
+} from './generate-images.mjs';
+//import { setInkColor, toggleDrawCanvas } from './utils/draw.mjs';
+
+const pageEl = document.querySelector('.page-a');
+
+const setTextareaStyle = (attrib, v) => (pageEl.style[attrib] = v);
+
+let inkColor = '#000f55';
+
+function setInkColor(color) {
+  inkColor = color;
+}
+
+/**
+ * Add event listeners here, they will be automatically mapped with addEventListener later
+ */
+const EVENT_MAP = {
+  '#generate-image-form': {
+    on: 'submit',
+    action: (e) => {
+      e.preventDefault();
+      generateImages();
+    }
+  },
+  '#font-size': {
+    on: 'change',
+    action: (e) => setTextareaStyle('fontSize', e.target.value + 'pt')
+  },
+  '#letter-spacing': {
+    on: 'change',
+    action: (e) => setTextareaStyle('letterSpacing', e.target.value + 'pt')
+  },
+  '#word-spacing': {
+    on: 'change',
+    action: (e) => setTextareaStyle('wordSpacing', e.target.value + 'px')
+  },
+  '#top-padding': {
+    on: 'change',
+    action: (e) => {
+      document.querySelector('.page-a .paper-content').style.paddingTop =
+        e.target.value + 'px';
+    }
+  },
+  '#ink-color': {
+    on: 'change',
+    action: (e) => {
+      document.body.style.setProperty('--ink-color', e.target.value);
+      setInkColor(e.target.value);
+    }
+  },
+  '#paper-margin-toggle': {
+    on: 'change',
+    action: () => {
+      if (pageEl.classList.contains('margined')) {
+        pageEl.classList.remove('margined');
+      } else {
+        pageEl.classList.add('margined');
+      }
+    }
+  },
+  '#paper-line-toggle': {
+    on: 'change',
+    action: () => {
+      if (pageEl.classList.contains('lines')) {
+        pageEl.classList.remove('lines');
+      } else {
+        pageEl.classList.add('lines');
+      }
+    }
+  },
+  /*
+  '#draw-diagram-button': {
+    on: 'click',
+    action: () => {
+        toggleDrawCanvas();
+    }
+  },
+  '.draw-container .close-button': {
+    on: 'click',
+    action: () => {
+      toggleDrawCanvas();
+    }
+  },*/
+  '#download-as-pdf-button': {
+    on: 'click',
+    action: () => {
+      downloadAsPDF();
+    }
+  },
+  '#delete-all-button': {
+    on: 'click',
+    action: () => {
+      deleteAll();
+    }
+  },
+  '.page-a .paper-content': {
+    on: 'paste',
+    action: formatText
+  },
+  
+  
+};
+
+for (const eventSelector in EVENT_MAP) {
+  document
+    .querySelector(eventSelector)
+    .addEventListener(
+      EVENT_MAP[eventSelector].on,
+      EVENT_MAP[eventSelector].action
+    );
+}
+
+/**
+ * This makes toggles, accessible.
+ */
+document.querySelectorAll('.switch-toggle input').forEach((toggleInput) => {
+  toggleInput.addEventListener('change', (e) => {
+    if (toggleInput.checked) {
+      document.querySelector(
+        `label[for="${toggleInput.id}"] .status`
+      ).textContent = 'on';
+      toggleInput.setAttribute('aria-checked', true);
+    } else {
+      toggleInput.setAttribute('aria-checked', false);
+      document.querySelector(
+        `label[for="${toggleInput.id}"] .status`
+      ).textContent = 'off';
+    }
+  });
+});
